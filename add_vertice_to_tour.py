39c9de7f8cafdfe9faf34a92c6da7bfc252034be
Jingqi Huang <huangjq_rose@berkeley.edu>
2019-12-05 15:01:14 -0800

    add_vertice

--- add_vertice_to_tour.py
+++ add_vertice_to_tour.py
@@ -1,21 +1,30 @@
 from student_utils import *
 
 def add_vertices_to_result(path, graph) :
-    path_result[]
+    path_result = []
     path_result_set = set(path_result)
     start_index = 0
     end_index = 1
     while end_index < len(path):
-        if path[end_index] in path_result_set:
+        if path[end_index] in path_result_set and path[end_index] != path[0]:
             end_index += 1
         else:
-            path_between_two_points = shortestPath(graph, path[start_index], path[end_index];
+            path_between_two_points = shortestPath(graph, path[start_index], path[end_index])
+            #print(path[start_index])
+            #print(path[end_index])
             path_result.extend(path_between_two_points[:-1])
             path_result_set = set(path_result);
-            start_index += 1
-            end_index = start_index + 1
+            start_index = end_index
+            end_index += 1
     path_result.append(path[-1])
+    #print(path_result)
+    return path_result
 
-path_1 = [3,1,4,2]
+
+
+
+#path_1 = [2,0,3,1,2]
+path_1 = [3,1,2,0,3]
+
 graph_1 = [[0,1,1,float('inf')], [1,0,1,float('inf')], [1,1,0,1], [float('inf'), float('inf'), 1,0]]
 add_vertices_to_result(path_1, graph_1)
